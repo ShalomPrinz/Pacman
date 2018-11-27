d062e8fd5b1257db6ead0dfb032ab9f982b9cedc
ShalomPrinz <44635075+ShalomPrinz@users.noreply.github.com>
2018-11-27 16:56:25 +0200

    creating a board with one pacman

--- Game.java
+++ Game.java
@@ -1,6 +1,30 @@
 
 public class Game {
-	public int getBoardDimensions(){
-		return 3030;
+	
+	enum Creatures {
+		Pacman 	
 	}
+	
+	Creatures[][] board = new Creatures[30][30];
+	
+	public int getBoardDimensions(){
+		return board.length * 100 + board[0].length;
+	}
+	
+	public Creatures[][] getBoard(){
+		board[23][15] = Creatures.Pacman;
+		return board;
+	}
+	
+	public int count(Creatures c){
+		int cNum = 0;
+		for (int i = 0; i < board.length; i++){
+			for (int j = 0; j < board[0].length; j++){
+				if (board[i][j] == c)
+					cNum ++;
+			}
+		}
+		return cNum;
+	}
+
 }
