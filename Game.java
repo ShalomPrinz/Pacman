140ad51e9a146444029f37890391cbf58d16aecd
ShalomPrinz <44635075+ShalomPrinz@users.noreply.github.com>
2018-11-27 17:07:14 +0200

    ghosts added to board

--- Game.java
+++ Game.java
@@ -2,7 +2,8 @@
 public class Game {
 	
 	enum Creatures {
-		Pacman 	
+		Pacman,
+		Ghost
 	}
 	
 	Creatures[][] board = new Creatures[30][30];
@@ -13,10 +14,15 @@ public class Game {
 	
 	public Creatures[][] getBoard(){
 		board[23][15] = Creatures.Pacman;
+		board[13][15] = Creatures.Ghost;
+		board[16][14] = Creatures.Ghost;
+		board[16][15] = Creatures.Ghost;
+		board[16][16] = Creatures.Ghost;
 		return board;
 	}
 	
 	public int count(Creatures c){
+		board = getBoard();
 		int cNum = 0;
 		for (int i = 0; i < board.length; i++){
 			for (int j = 0; j < board[0].length; j++){
