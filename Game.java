2fe9f9f9d116c0223f5855d8a19449da6d442b8b
ShalomPrinz <44635075+ShalomPrinz@users.noreply.github.com>
2018-11-27 17:33:48 +0200

    fillBoardRaw function
    Instead of filling the board step by step, this function will help me now with the points (328!)

--- Game.java
+++ Game.java
@@ -12,17 +12,16 @@ public class Game {
 		return board.length * 100 + board[0].length;
 	}
 	
-	public Creatures[][] getBoard(){
+	public Creatures[][] setBoard(){
 		board[23][15] = Creatures.Pacman;
 		board[13][15] = Creatures.Ghost;
-		board[16][14] = Creatures.Ghost;
-		board[16][15] = Creatures.Ghost;
-		board[16][16] = Creatures.Ghost;
+		fillBoardRaw(16, 14, 16, Creatures.Ghost);
+		
 		return board;
 	}
 	
 	public int count(Creatures c){
-		board = getBoard();
+		board = setBoard();
 		int cNum = 0;
 		for (int i = 0; i < board.length; i++){
 			for (int j = 0; j < board[0].length; j++){
@@ -32,5 +31,14 @@ public class Game {
 		}
 		return cNum;
 	}
+	
+	private void fillBoardRaw(int raw, int start, int end, Creatures c){
+		if (end < start || raw < 0 || raw > 30)
+			return;
+		for (int i = start; i <= end; i++)
+			board[raw][i] = c;
+	}
+	
+	
 
 }
