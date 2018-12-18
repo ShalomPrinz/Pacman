0917cf33bba20a20b1c0491b9f64d2c1ce296c3c
ShalomPrinz <44635075+ShalomPrinz@users.noreply.github.com>
2018-12-18 20:43:42 +0200

    Update BoardTest.java
    1 - The game board is now private, so i save it in the test class.
    2 - count method is in the test class now, not needed in the game class
    3 - New test: move. checking if there was a movement

--- BoardTest.java
+++ BoardTest.java
@@ -6,10 +6,12 @@ import org.junit.Test;
 public class BoardTest {
 	
 	Game game;
+	Game.Creatures[][] board;
 	
 	@Before
 	public void setup(){
 		game = new Game();
+		board = game.getBoard();
 	}
 	
 	@Test
@@ -24,58 +26,80 @@ public class BoardTest {
 	}
 	
 	@Test
-	public void testPacman() throws Exception{
+	public void testPacman(){
 		// arrange
 		
 		// action
-		int pacmans = game.count(Game.Creatures.Pacman);
+		int pacmans = count(Game.Creatures.Pacman);
 		
 		// assert
 		assertEquals(1, pacmans);
 	}
 	
 	@Test
-	public void testGhosts() throws Exception{
+	public void testGhosts(){
 		// arrange
 		
 		// action
-		int ghosts = game.count(Game.Creatures.Ghost);
+		int ghosts = count(Game.Creatures.Ghost1) + count(Game.Creatures.Ghost2) +
+					 count(Game.Creatures.Ghost3) + count(Game.Creatures.Ghost4);
 		
 		// assert
 		assertEquals(4, ghosts);
 	}
 	
 	@Test
-	public void testPoints() throws Exception{
+	public void testPoints(){
 		// arrange
 		
 		// action
-		int points = game.count(Game.Creatures.Point);
+		int points = count(Game.Creatures.Point);
 		
 		// assert
 		assertEquals(330, points);
 	}
 	
 	@Test
-	public void testNulls() throws Exception{
+	public void testNulls(){
 		// arrange
 		
 		// action
-		int nulls = game.count(Game.Creatures.Null);
+		int nulls = count(Game.Creatures.Null);
 		
 		// assert
 		assertEquals(148, nulls);
 	}
 	
 	@Test
-	public void testWalls() throws Exception{
+	public void testWalls(){
 		// arrange
 		
 		// action
-		int walls = game.count(Game.Creatures.Wall);
+		int walls = count(Game.Creatures.Wall);
 		
 		// assert
 		assertEquals(417, walls);
 	}
 	
+	private int count(Game.Creatures c){
+		int cNum = 0;
+		for (int i = 0; i < board.length; i++){
+			for (int j = 0; j < board[0].length; j++){
+				if (board[i][j] == c)
+					cNum ++;
+			}
+		}
+		return cNum;
+	}	
+	
+	@Test
+	public void moveCreature(){
+		// arrange
+		
+		// action
+		boolean result = game.move(Game.Creatures.Pacman, Game.Directions.RIGHT);
+		
+		// assert	
+		assertTrue(result);	
+	}
 }
