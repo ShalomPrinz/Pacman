d197abefc085c32340b13213ec255ab8c9e1fdd2
ShalomPrinz <44635075+ShalomPrinz@users.noreply.github.com>
2018-12-18 20:50:24 +0200

    Update Game.java
    1 - In the Creatures enum, every ghost now is a different Creature. -> To find a specific ghost to move it
    2 - New enum: directions, for the method move
    3 - Move method. It gets a specific creature and a direction, and if it's possible - the creature will move 1 spot(?) to the direction

--- Game.java
+++ Game.java
@@ -1,70 +1,126 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
 public class Game {
 	
+	public Game(){
+		try{
+			this.board = setBoard();
+		}
+		catch(FileNotFoundException e){
+			// no such file
+		}
+	}
+	
 	enum Creatures {
 		Pacman,
-		Ghost,
+		Ghost1, Ghost2, Ghost3, Ghost4,
 		Point,
 		Wall,
 		Null
 	}
 	
-	Creatures[][] board = new Creatures[30][30];
+	enum Directions {
+		RIGHT,
+		LEFT,
+		UP,
+		DOWN
+	}
 	
+	private Creatures[][] board = new Creatures[30][30];
+	int ghostNum;
+	
+	public Creatures[][] getBoard() {
+		return board;
+	}
+
 	public int getBoardDimensions(){
 		return board.length * 100 + board[0].length;
 	}
 	
-	public Creatures[][] setBoard() throws Exception{
-		// Change Board.txt path on another computer.
-		// Home path: E:/Docs/שלום/A android project 2018/Pacman/Board.txt
-		Scanner sc = new Scanner(new BufferedReader(new FileReader("E:/Docs/שלום/A android project 2018/Pacman/Board.txt")));
+	public Creatures[][] setBoard() throws FileNotFoundException{
+		this.ghostNum = 0;
+		Scanner sc = new Scanner(new BufferedReader(new FileReader("./Board.txt")));
 	    while(sc.hasNextLine()) {
 	    	for (int i = 0; i < board.length && sc.hasNextLine(); i++) {
     			String[] line = sc.nextLine().trim().split("	");
-    			line = reverseLine(line);
 	        	for (int j = 0; j < line.length; j++) 
 	        		board[i][j] = StringToCreature(line[j]); 
 	        }
 	    }
 	    sc.close();
+	    this.ghostNum = 0;
 	    return board;
 	}
 	
-	public int count(Creatures c) throws Exception{
-		board = setBoard();
-		int cNum = 0;
-		for (int i = 0; i < board.length; i++){
-			for (int j = 0; j < board[0].length; j++){
-				if (board[i][j] == c)
-					cNum ++;
-			}
-		}
-		return cNum;
-	}	
-	
 	private Creatures StringToCreature(String st){
 		switch(st){
-		case "G":
-			return Creatures.Ghost;
-		case "P":
-			return Creatures.Pacman;
-		case "W":
-			return Creatures.Wall;
-		case "-":
-			return Creatures.Point;
-		default:
-			return Creatures.Null;
+			case "P":
+				return Creatures.Pacman;
+			case "W":
+				return Creatures.Wall;
+			case "-":
+				return Creatures.Point;
+			case "G":
+				this.ghostNum++;
+				switch(this.ghostNum){
+					case 1:
+						return Creatures.Ghost1;
+					case 2:
+						return Creatures.Ghost2;
+					case 3:
+						return Creatures.Ghost3;
+					case 4:
+						return Creatures.Ghost4;
+				}
+			default:
+				return Creatures.Null;
 		}
 	}
-	
-	private String[] reverseLine(String[] line){
-		String newLine[] = new String[line.length];
-		for (int i = 0; i < line.length; i++)
-			newLine[i] = line[line.length - 1 - i];
-		return newLine;
+
+	public boolean move(Creatures c, Directions d){
+		int line = find(c)[0];
+		int column = find(c)[1];
+		int Nline = changeLocationByDirection(d, line, column)[0];
+		int Ncolumn = changeLocationByDirection(d, line, column)[1];
+
+		if (this.board[Nline][Ncolumn] == Creatures.Wall)
+			return false;
+		
+		Creatures temp = this.board[Nline][Ncolumn];
+		this.board[Nline][Ncolumn] = c;
+		this.board[line][column] = temp;
+		return true;
 	}
+	
+	private int[] changeLocationByDirection(Directions d, int line, int column){
+		switch (d){
+			case DOWN:
+				line++;
+				break;
+			case UP:
+				line--;
+				break;
+			case RIGHT:
+				column++;
+				break;
+			case LEFT:
+				column--;
+				break;
+		}
+		return new int[]{line, column};
+	}
+	
+	private int[] find(Creatures c){
+		for (int i = 0; i < this.board.length; i++){
+			for (int j = 0; j < this.board[0].length; j++){
+				if (this.board[i][j] == c)
+					return new int[]{i, j};
+			}
+		}
+		return null;
+	}
+
 }
