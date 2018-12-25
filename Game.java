061f01cf431212f9a7dc2ea8d4d5533011b1eac5
ShalomPrinz <44635075+ShalomPrinz@users.noreply.github.com>
2018-12-25 18:14:32 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,7 +1,17 @@
 
 public class Game {
 	
-	Board.Creatures[][] board = new Board().getBoard();
+	Board.Creatures[][] board;
+	
+	Location[] savePoints;
+	
+	int pointsSavedNum;
+	
+	public Game(){
+		this.board = new Board().getBoard();
+		this.savePoints = new Location[]{null, null, null, null}; // 4 - Ghosts number
+		this.pointsSavedNum = 0;
+	}
 	
 	enum Directions{
 		Right,
@@ -35,6 +45,55 @@ public class Game {
 			break;
 		}
 
+	}
+	
+	public void moveGhost(Location l, Directions d){
+		// wall - nothing
+		int x = l.getX(), y = l.getY();
+		int nX = changeLocationByDirection(x, y, d).getX(),
+				nY = changeLocationByDirection(x, y, d).getY();
+		
+		if (this.board[nX][nY] == Board.Creatures.Point || 
+				this.board[nX][nY] == Board.Creatures.Null){
+			
+			this.board[x][y] = Board.Creatures.Null;
+			this.board[nX][nY] = Board.Creatures.Ghost1;
+			
+			// if point - add to save array
+			this.savePoints[pointsSavedNum] = new Location(nX, nY);
+			pointsSavedNum ++;
+		}
+		
+		relocatePoints();
+		
+		sortNullsOnArray();
+	}
+
+	public void sortNullsOnArray() {	
+		for (int i = 0; i < savePoints.length; i++){
+			for (int j = savePoints.length - 1; j > i; j--){
+				if (savePoints[i] == null){
+					savePoints[i] = savePoints[j];
+					savePoints[j] = null;
+				}
+				else
+					break;
+			}
+		}
+	}
+
+	public void relocatePoints() {
+		int originalSaves = this.pointsSavedNum;
+		
+		for (int i = 0; i < originalSaves; i++){
+			int x = savePoints[i].getX(), y = savePoints[i].getY();
+			
+			if (this.board[x][y] != Board.Creatures.Null) // checks if can relocate point
+				continue;
+			
+			this.board[x][y] = Board.Creatures.Point;
+			this.pointsSavedNum --;
+		}
 	}
 	
 	public Location changeLocationByDirection(int x, int y, Directions d){
