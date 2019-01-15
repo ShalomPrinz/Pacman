dfdda9cb301a9eb7dbf9fba856c978414c89d5b4
ShalomPrinz <44635075+ShalomPrinz@users.noreply.github.com>
2019-01-15 08:43:56 +0200

    Interface :: Moveable
    for creatures

---
+++ Moveable.java
@@ -0,0 +1,11 @@
+
+public interface Moveable {
+	
+	public Location getLocation();
+
+	public void setLocation(Location location);
+
+	public Game.Direction getDirection();
+
+	public void setDirection(Game.Direction direction);
+}
