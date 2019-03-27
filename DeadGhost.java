0f1432b202481227c09a379dfe2b82e3d47ec61c
Shalom Prinz <shalom.prinz@gmail.com>
2019-03-27 13:12:25 +0200

    added test: revivor is one step right from ghost

--- DeadGhost.java
+++ DeadGhost.java
@@ -1,6 +1,13 @@
 package pacman;
 
-public class DeadGhost{
+import pacman.Game.Direction;
+
+public class DeadGhost extends Ghost {
 	
+	public Direction findPath(Location goal) {
+		
+		return Direction.RIGHT;
+		
+	}
 	
 }
--- DeadGhostTest.java
+++ DeadGhostTest.java
@@ -4,11 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import pacman.Game.Direction;
+
 class DeadGhostTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void oneStepRight() {
+		Game game = new Game( new String[] {"GR"} );
+		Location rev = new Location(0, 1);
+		DeadGhost dg = new DeadGhost();
+		
+		Direction d = dg.findPath(rev);
+		
+		assertEquals(Direction.RIGHT, d);
 	}
 
 }
